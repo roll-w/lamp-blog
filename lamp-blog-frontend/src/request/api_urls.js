f07f34416a5bff87cac5b397c5c3de99734a0a81
RollW <73376341+Roll-W@users.noreply.github.com>
2023-11-26 17:35:41 +0800

    refactor: move URLs to api_urls.js.

---
+++ lamp-blog-frontend/src/request/api_urls.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2023 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const base = 'localhost:5100';
+const frontBase = 'localhost:5000';
+
+export const frontBaseUrl = `http://${frontBase}`;
+
+export const baseUrl = `http://${base}`;
+export const wsBaseUrl = `ws://${base}`;
+
+const prefix = `${baseUrl}/api/v1`;
+const adminPrefix = `${baseUrl}/api/v1/admin`;
+const wsPrefix = `ws://${base}/api/v1`;
+
+export const passwordLogin = `${prefix}/user/login/password`;
+export const emailLogin = `${prefix}/user/login/email`;
+export const logout = `${prefix}/user/logout`;
+export const register = `${prefix}/user/register`;
+export const registerActivate = (token) =>
+    `${prefix}/user/register/token/${token}`;
+
+export const user = (admin = false) =>
+    `${admin ? adminPrefix : prefix}/user`;
+export const users = (admin = false, id = null) =>
+    `${admin ? adminPrefix : prefix}/users${id ? `/${id}` : ''}`;
+export const searchUsers = (admin = false) =>
+    `${admin ? adminPrefix : prefix}/users/search`;
+export const currentUser = `${prefix}/user`;
+
+
+export const userChat = wsPrefix + '/message/talk';
+export const userMessages = (userId, admin = false) =>
+    `${admin ? adminPrefix : prefix}/user/${userId}/messages`;
+export const messages = (admin = false) =>
+    `${admin ? adminPrefix : prefix}/messages`;
+
+export const staffs = `${adminPrefix}/staffs`;
+export const staffInfo = (staffId, admin = false) =>
+    `${prefix}/staff/${staffId}`;
+
+export const staffInfoByUser = (userId, admin = false) =>
+    `${prefix}/user/${userId}/staff`;
+
+export const allReviews = `${adminPrefix}/reviews`;
+export const currentReviews = `${prefix}/reviews`;
+
+export const statusReviews = (userId, admin = false) =>
+    `${admin ? adminPrefix : prefix}/${userId}/reviews`;
+export const reviewContent = (jobId, admin = false) =>
+    `${admin ? adminPrefix : prefix}/review/${jobId}/content`;
+export const reviewResource = (jobId, admin = false) =>
+    `${admin ? adminPrefix : prefix}/review/${jobId}`;
+
+
+export const tokenRefresh = `${prefix}/auth/token/r`;
+
+export const articleDetails = (userId, articleId, admin) => {
+    return `${admin ? adminPrefix : prefix}/user/${userId}/articles/${articleId}`;
+}
+export const userArticles = (userId, admin = false) => {
+    return `${admin ? adminPrefix : prefix}/user/${userId}/articles`;
+}
+export const articles = (admin) => `${admin ? adminPrefix : prefix}/articles`;
+
+export const userList = `${adminPrefix}/users`;
+export const systemErrorLog = `${adminPrefix}/system/errors`;
+export const systemSettings = `${adminPrefix}/system/settings`;
+export const systemMessageResources = `${adminPrefix}/system/resources/messages`;
