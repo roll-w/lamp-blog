a70ef171481d736280eb0670dba9ae1d82a2cece
RollW <73376341+Roll-W@users.noreply.github.com>
2024-08-11 13:23:19 +0800

    feat: initially implement HTTP APIs of comment service.

---
+++ lamp-blog-web/src/main/java/space/lingu/lamp/web/controller/comment/CommentController.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2023 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package space.lingu.lamp.web.controller.comment
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import space.lingu.lamp.web.common.ApiContext
+import space.lingu.lamp.web.controller.Api
+import space.lingu.lamp.web.controller.comment.model.CommentRequest
+import space.lingu.lamp.web.controller.comment.model.CommentVo
+import space.lingu.lamp.web.domain.comment.CommentDetailsMetadata
+import space.lingu.lamp.web.domain.content.ContentPublishProvider
+import space.lingu.lamp.web.domain.content.ContentType
+import space.lingu.lamp.web.domain.content.SimpleUncreatedContent
+import space.lingu.lamp.web.domain.content.collection.ContentCollectionType
+import tech.rollw.common.web.HttpResponseEntity
+import tech.rollw.common.web.ParameterFailedException
+import tech.rollw.common.web.system.ContextThreadAware
+
+/**
+ * @author RollW
+ */
+@Api
+class CommentController(
+    private val apiContextThreadAware: ContextThreadAware<ApiContext>,
+    private val contentPublishProvider: ContentPublishProvider
+) {
+    @PostMapping("/{contentType}/{contentId}/comments")
+    fun createComment(
+        @PathVariable("contentId") contentId: Long,
+        @PathVariable("contentType") type: String,
+        @RequestBody commentRequest: CommentRequest
+    ): HttpResponseEntity<CommentVo> {
+        val context = apiContextThreadAware.contextThread
+            .context
+        val user = context.user!!
+
+        val contentType = getContentType(type)
+        val commentDetailsMetadata = CommentDetailsMetadata(
+            contentType, contentId,
+            commentRequest.parent
+        )
+        val uncreatedContent = SimpleUncreatedContent(
+            ContentType.COMMENT,
+            user.userId,
+            null,
+            commentRequest.content,
+            commentDetailsMetadata
+        )
+        val contentDetails = contentPublishProvider.publishContent(
+            uncreatedContent
+        )
+        return HttpResponseEntity.success(
+            CommentVo.of(contentDetails)
+        )
+    }
+
+    // TODO: implement the following methods
+    @GetMapping("/{contentType}/{contentId}/comments")
+    fun getComments(
+        @PathVariable("contentId") contentId: Long?,
+        @PathVariable("contentType") type: String
+    ): HttpResponseEntity<List<CommentVo>> {
+        val contentType = getContentType(type)
+        val collectionType = getFromContentType(contentType)
+
+        return HttpResponseEntity.success(listOf())
+    }
+
+
+    @GetMapping("/user/comments")
+    fun getCommentsOfCurrentUser(): HttpResponseEntity<List<CommentVo>> {
+        return HttpResponseEntity.success(listOf())
+    }
+
+    @GetMapping("/users/{userId}/comments")
+    fun getCommentsOfUser(@PathVariable("userId") userId: Long): HttpResponseEntity<List<CommentVo>> {
+        return HttpResponseEntity.success(listOf())
+    }
+
+    private fun getFromContentType(contentType: ContentType): ContentCollectionType {
+        return when (contentType) {
+            ContentType.ARTICLE -> ContentCollectionType.ARTICLE_COMMENTS
+            ContentType.POST -> ContentCollectionType.POST_COMMENTS
+            ContentType.COMMENT -> ContentCollectionType.COMMENTS
+            else -> throw IllegalArgumentException("Unsupported content type: $contentType")
+        }
+    }
+
+    companion object {
+        private fun getContentType(contentType: String): ContentType =
+            ContentType.findByName(contentType)
+                ?: throw ParameterFailedException("Content type not found.")
+
+    }
+}
---
+++ lamp-blog-web/src/main/java/space/lingu/lamp/web/controller/comment/model/CommentVo.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package space.lingu.lamp.web.controller.comment.model
+
+import space.lingu.lamp.web.controller.content.vo.ContentVo
+import space.lingu.lamp.web.domain.comment.Comment
+import space.lingu.lamp.web.domain.content.ContentDetails
+import space.lingu.lamp.web.domain.content.ContentType
+
+/**
+ * @author RollW
+ */
+data class CommentVo(
+    val id: Long,
+    val userId: Long,
+    val parent: Long,
+    val content: String,
+    val contentId: Long,
+    val contentType: ContentType,
+    val createTime: Long,
+    val updateTime: Long
+) : ContentVo {
+
+    override fun id(): Long = id
+
+    override fun createTime(): Long = createTime
+
+    override fun updateTime(): Long = updateTime
+
+    companion object {
+        @JvmStatic
+        fun of(contentDetails: ContentDetails?): CommentVo? {
+            if (contentDetails == null) {
+                return null
+            }
+            if (contentDetails !is Comment) {
+                return null
+            }
+            return CommentVo(
+                contentDetails.id,
+                contentDetails.getUserId(),
+                contentDetails.parentId,
+                contentDetails.getContent(),
+                contentDetails.commentOn,
+                contentDetails.type,
+                contentDetails.getCreateTime(),
+                contentDetails.getUpdateTime()
+            )
+        }
+    }
+
+}
