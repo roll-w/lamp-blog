4cd0a0c52f8abf395ba0b4d3bed6b5bbac466f06
RollW <73376341+Roll-W@users.noreply.github.com>
2024-08-12 22:49:23 +0800

    feat: implementation of article ContentProvider.

---
+++ lamp-blog-web/src/main/java/space/lingu/lamp/web/domain/article/service/ArticleProviderService.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2023 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package space.lingu.lamp.web.domain.article.service
+
+import org.springframework.stereotype.Service
+import space.lingu.NonNull
+import space.lingu.lamp.web.domain.article.Article
+import space.lingu.lamp.web.domain.article.repository.ArticleRepository
+import space.lingu.lamp.web.domain.content.ContentDetails
+import space.lingu.lamp.web.domain.content.ContentOperator
+import space.lingu.lamp.web.domain.content.ContentProvider
+import space.lingu.lamp.web.domain.content.ContentTrait
+import space.lingu.lamp.web.domain.content.ContentType
+import space.lingu.lamp.web.domain.content.common.ContentErrorCode
+import space.lingu.lamp.web.domain.content.common.ContentException
+import space.lingu.lamp.web.domain.content.service.ContentMetadataService
+
+/**
+ * @author RollW
+ */
+@Service
+class ArticleProviderService(
+    private val articleRepository: ArticleRepository,
+    override val contentMetadataService: ContentMetadataService
+) : ContentProvider,
+    ArticleOperatorDelegate {
+    override fun supports(@NonNull contentType: ContentType): Boolean {
+        return contentType == ContentType.ARTICLE
+    }
+
+    override fun getContentDetails(@NonNull contentTrait: ContentTrait): ContentDetails {
+        return articleRepository.getById(contentTrait.contentId)
+            ?: throw ContentException(ContentErrorCode.ERROR_CONTENT_NOT_FOUND)
+    }
+
+    override fun getContentOperator(@NonNull contentTrait: ContentTrait, checkDelete: Boolean): ContentOperator {
+        val article = articleRepository.getById(contentTrait.contentId)
+            ?: throw ContentException(ContentErrorCode.ERROR_CONTENT_NOT_FOUND)
+
+        return ArticleOperatorImpl(article, this, checkDelete)
+    }
+
+    override fun updateArticle(article: Article) {
+        articleRepository.update(article)
+    }
+}
